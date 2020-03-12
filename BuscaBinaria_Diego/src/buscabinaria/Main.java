b34e586d8c3c1eef7fbaf9d4657280e04ddc6185
Diego Liebermann <37605030+Diegobrvt@users.noreply.github.com>
2020-03-12 09:46:52 -0300

    Add files via upload

---
+++ BuscaBinaria_Diego/src/buscabinaria/Main.java
@@ -0,0 +1,12 @@
+package cepordenado;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception 
+	{		
+		Procurar procurar = new Procurar("88301041");
+		
+		procurar.retornaResultado();
+	}
+
+}
